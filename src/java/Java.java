24d4c2dac6e24991499fa928758e7aca81468bd3
admin <fsyjs_js@hotmail.com>
2016-11-17 15:25:21 +0800

    add testing branches message

--- src/java/Java.java
+++ src/java/Java.java
@@ -16,6 +16,8 @@ public class Java {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("this is java");
+        System.out.println("this is testing branches");
     }
     
 }
