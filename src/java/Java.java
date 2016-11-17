539ae348e909004e3ec0a2403ad200843b6508c4
admin <fsyjs_js@hotmail.com>
2016-11-17 16:01:33 +0800

    third branches message added

--- src/java/Java.java
+++ src/java/Java.java
@@ -15,7 +15,10 @@ public class Java {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic 
+        
+        
+        System.out.println("this is third branches");
     }
     
 }
